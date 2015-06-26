2ac79c4eb339014426e8407300136163902e1686
Andrew Hudson <ahudson@gmail.com>
2015-06-26 15:44:25 +0100

    made good progress

---
+++ README.md
@@ -0,0 +1,3 @@
+# postcss-test
+
+This is where I am going to be testing out postcss. 
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,9 @@
 /* global require */
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	jshint = require('gulp-jshint'),
-	stripDebug = require('gulp-strip-debug'),
 	sass = require('gulp-sass'),
-	scsslint = require('gulp-scss-lint'),
-	autoprefix = require('gulp-autoprefixer'),
-	minifyCSS = require('gulp-minify-css'),
 	livereload = require('gulp-livereload'),
-	stylish = require('jshint-stylish'),
-	uncss = require('gulp-uncss'),
-	penthouse = require('penthouse'),
-	Promise = require('bluebird'),
-	penthouseAsync = Promise.promisify(penthouse),
-	phpcs = require('gulp-phpcs'),
-	critical = require('critical'),
-	pages = [
-		'http://big-andy.dev/contact/',
-		'http://big-andy.dev/cv/',
-		'http://big-andy.dev/about/',
-		'http://big-andy.dev/photos/',
-		'http://big-andy.dev/',
-		'http://big-andy.dev/blog',
-		'http://big-andy.dev/style-guide',
-		'http://big-andy.dev/https/',
-		'http://big-andy.dev/breaking-borders-3/'
-	],
+
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer-core'),
 	mqpacker = require('css-mqpacker'),
@@ -40,153 +16,43 @@ var gulp = require('gulp'),
 	fs = require('fs'),
 	inputCss = fs.readFileSync('postcss/style.css', 'utf8'),
 	postcssRoot = require('postcss'),
-	cssnext = require('gulp-cssnext');
+	cssnext = require('gulp-cssnext'),
+	simpleExtend = require('postcss-simple-extend'),
+	focus = require('postcss-focus');
 
-gulp.task('critical-css', function() {
-	penthouseAsync({
-		url: pages,
-		css: './style.css',
-		height: 600,
-		width: 400
-	}).then( function (criticalCSS){
-		require('fs').writeFile('build/css/critical.css', criticalCSS);
-	});
-});
 
-gulp.task('uncss', function() {
-	return gulp.src('./style.css')
-		.pipe(uncss({
-			html: pages,
-			ignore: [
-				'[data-visited]',
-				'[data-visited] .post-content'
-			]
-		}))
-		.pipe(minifyCSS({
-				keepSpecialComments: 0
-			}))
-		.pipe(autoprefix('last 2 versions'))
-		.pipe(gulp.dest('.'));
-});
-
-gulp.task('css', function () {
+gulp.task('postcss', function () {
 	var processors = [
 		autoprefixer({browsers: ['last 1 version']}),
+		postcssImport,
 		mixins,
 		mqpacker,
 		csswring,
+		simpleExtend,
 		nestedcss,
 		vars({ variables: colours }),
-		postcssImport,
-		cssnext
+		focus,
+
+
+		// cssnext({
+		// 	browsers: ('last 1 version'),
+		// 	compress: true,
+		// 	sourcemap: true
+		// })
 	];
-
-	return gulp
-		.src('postcss/style.css')
+	return gulp.src(['./postcss/style.css', './postcss/font.css'])
 		.pipe(postcss(processors))
-		.on('error', function (error) {
-		  console.log(error)
-		})
-		.pipe(gulp.dest('build/postcss'))
-});
-
-// concat and minify the js
-gulp.task('js', ['js-lint'], function () {
-	gulp.src([
-			// 'js/google-analytics-caller.js',
-			'js/lazy-load-css.js',
-			'js/main.js',
-		])
-		.pipe(gutil.env.type === 'production' ? stripDebug() : gutil.noop())
-		.pipe(uglify())
-		.pipe(concat('script.min.js'))
-		.pipe(gulp.dest('build/js'));
-
-	gulp.src([
-		'bower_components/jquery/dist/jquery.js',
-		'bower_components/picturefill/dist/picturefill.min.js',
-		'bower_components/lazyloadxt/dist/jquery.lazyloadxt.js',
-		'bower_components/lazyloadxt/dist/jquery.lazyloadxt.srcset.js'
-	])
-		.pipe(uglify())
-		.pipe(concat('picturefill.min.js'))
-		.pipe(gulp.dest('build/js'));
-	gulp.src(['js/analytics.js'])
-		.pipe(gulp.dest('build/js'));
-	gulp.src(['js/font-loader.js'])
-		.pipe(uglify())
-		.pipe(gulp.dest('build/js'));
-
-});
-
-gulp.task('js-lint', function() {
-	gulp.src([
-			'js/main.js'
-		])
-		.pipe(jshint('.jshint'))
-		.pipe(jshint.reporter(stylish));
-});
-
-gulp.task('wordpress-lint', function () {
-	return gulp.src(['./**/*.php', '!node_modules/**/*.php'])
-		.pipe(phpcs({
-			standard: 'code.ruleset.xml'
-		}))
-		.pipe(phpcs.reporter('log'));
-});
-
-// sass
-gulp.task('sass', function () {
-	var processors = [
-		autoprefixer({browsers: ['last 1 version']}),
-	];
-
-	gulp.src('./scss/**/*.scss')
-		.pipe(sass({
-			includePaths: ['bower_components/foundation/scss'],
-			// errLogToConsole: true,
-			// outputStyle: 'compressed'
-		}))
-
-		.pipe(cssnext({
-			browsers: ('last 1 version'),
-			compress: true,
-			sourcemap: true
-		}))
-
-		.pipe(postcss(processors))
-		.pipe(gulp.dest('.'));
-
-	gulp.src('./scss/font.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}))
-		.pipe(gulp.dest('./build/css'));
-});
-
-gulp.task('scss-lint', function () {
-	gulp.src([
-			'scss/**/*.scss',
-			'!scss/style.scss', // ignore this file so can include commenting in it.
-			'!scss/font.scss'
-		])
-		.pipe(scsslint({
-			'config': '.scss-lint.yml',
-		}));
+		.pipe(gulp.dest('./build'));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
-	gulp.watch('js/*', ['js']);
-	gulp.watch('scss/**/*', ['sass']);
-	// gulp.watch('images/svg/*.svg', ['sprites']);
+	gulp.watch('postcss/**/*', ['postcss']);
 
 	var server = livereload();
 	gulp.watch([
 		'style.css',
-		'build/**',
-		'*.php',
-		'scss/**'
+		'postcss/**'
 	]).on('change', function(file) {
 		server.changed(file.path);
 	});
@@ -194,21 +60,6 @@ gulp.task('watch', function () {
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', [
-	'js',
-	'sass',
+	'postcss',
 	'watch',
 ]);
-
-gulp.task('production', [
-	'js', 'sass'
-]);
-
-gulp.task('deploy', [
-	'sass',
-	'uncss',
-	'js',
-	'lint',
-	'critical-css'
-]);
-
-gulp.task('lint', ['scss-lint', 'js-lint', 'wordpress-lint']);
